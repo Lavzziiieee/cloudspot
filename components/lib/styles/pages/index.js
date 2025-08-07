31824e385091734ba4ab6d10237c04656558154a
Lavzziiieee <lavanyasweety138@gmail.com>
2025-08-07 15:21:10 +0530

    Update index.js

--- components/lib/styles/pages/index.js
+++ components/lib/styles/pages/index.js
@@ -1,1 +1,21 @@
-pages/index.js
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { getAuth } from "firebase/auth";
+import { app } from "../lib/firebase";
+
+export default function Home() {
+  const router = useRouter();
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) router.push("/dashboard");
+      else router.push("/login");
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return <p>Redirecting...</p>;
+}
+
