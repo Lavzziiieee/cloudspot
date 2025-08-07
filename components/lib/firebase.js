4618b137db9409c343445ad7821f5ef5d1540e37
Lavzziiieee <lavanyasweety138@gmail.com>
2025-08-07 15:19:50 +0530

    Update firebase.js

--- components/lib/firebase.js
+++ components/lib/firebase.js
@@ -1,1 +1,14 @@
-lib/firebase.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8nlnRkVqlOLRwYtMhNv5nZNarWzQcdO0",
+  authDomain: "cloudspot-6c5b4.firebaseapp.com",
+  projectId: "cloudspot-6c5b4",
+  storageBucket: "cloudspot-6c5b4.appspot.com",
+  messagingSenderId: "712370108945",
+  appId: "1:712370108945:web:d4c6ebf2a5af2c9c718fb0",
+  measurementId: "G-7XFBRCB9WP"
+};
+
+export const app = initializeApp(firebaseConfig);
+
